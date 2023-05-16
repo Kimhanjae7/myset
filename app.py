80543969ab9aaf0ad86aa27f55dedf4356c1bef0
Kimhanjae7 <126743737+Kimhanjae7@users.noreply.github.com>
2023-05-16 11:21:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,3 @@ st.write(x,'squared is',x * x)
 st.sidebar.title("This is written inside th sidebar")
 st.sidebar.button("Click")
 st.sidebar.radio("Pick your gender",["Male","Female"])     
-
-st.graphviz_chart('''
-    digraph {
-        Big_shark -> Tuna
-        Tuna -> Mackerel
-        Mackerel -> Small_fishes
-        Small_fishes -> Shrimp
-    }
-''')

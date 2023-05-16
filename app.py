e33aa91cfa26a7c6d96aee68925d1602f6a7a791
Kimhanjae7 <126743737+Kimhanjae7@users.noreply.github.com>
2023-05-16 11:29:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,12 @@ st.write(x,'squared is',x * x)
 
 st.sidebar.title("This is written inside th sidebar")
 st.sidebar.button("Click")
-st.sidebar.radio("Pick your gender",["Male","Female"])     
+st.sidebar.radio("Pick your gender",["Male","Female"])
+
+st.number_input('Pick a number',0,10)
+st.text_input('Email adress')
+st.date_input('Travelling date')
+st.time_input('school time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favouriote color')

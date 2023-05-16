74e0b7c8f4e3fbeb5f27f6598260a791d2f6c012
Kimhanjae7 <126743737+Kimhanjae7@users.noreply.github.com>
2023-05-16 11:21:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import graphviz as graphviz
 
 x = st.slider('Select a value')
 st.write(x,'squared is',x * x)
 
 st.sidebar.title("This is written inside th sidebar")
 st.sidebar.button("Click")
-st.sidebar.radio("Pick your gender",["Male","Female"])                
+st.sidebar.radio("Pick your gender",["Male","Female"])     
+
+st.graphviz_chart('''
+    digraph {
+        Big_shark -> Tuna
+        Tuna -> Mackerel
+        Mackerel -> Small_fishes
+        Small_fishes -> Shrimp
+    }
+''')
